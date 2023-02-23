0a312b455caba1f54e05490cd7cd50479dacef28
Kevin Grimaldi <anthgrim@icloud.com>
2023-02-23 15:02:44 -0500

    adding ingress

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,10 @@ const app = express()
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Hi There!')
+})
+
 app.listen(3000, () => {
   console.log('Auth Service running on port:3000')
 })
