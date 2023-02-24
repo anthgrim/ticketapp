66b80cb4b8bdd7ebdcb4dacb5e13052779df93a7
Kevin Grimaldi <anthgrim@icloud.com>
2023-02-24 15:24:04 -0500

    multiple changes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,20 @@
 import express from 'express'
+import { currentuserRouter } from './routes/current-user'
+import { siginRouter } from './routes/signin'
+import { signoutRouter } from './routes/signout'
+import { signupRouter } from './routes/signup'
 
 const app = express()
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi There!')
-})
+app.use(currentuserRouter)
+app.use(siginRouter)
+app.use(signoutRouter)
+app.use(signupRouter)
+
+process.on('uncaughtException', (err) => console.log('Error:', err.message))
 
 app.listen(3000, () => {
   console.log('Auth Service running on port:3000')
