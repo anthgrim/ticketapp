4776855a9316ddf6191bcc82119e7de305431bbd
Kevin Grimaldi <anthgrim@icloud.com>
2023-02-26 13:44:59 -0500

    adding custom error abstract class

---
+++ auth/src/errors/custom-error.ts
@@ -0,0 +1,15 @@
+interface SerializedResponse {
+  message: string
+  field?: string
+}
+
+export abstract class CustomError extends Error {
+  abstract statusCode: number
+
+  constructor() {
+    super()
+    Object.setPrototypeOf(this, CustomError.prototype)
+  }
+
+  abstract serializeErrors(): SerializedResponse[]
+}
