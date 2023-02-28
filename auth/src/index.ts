02a9283ffbb7455980b532c4ecaf24884e7a5ba1
Kevin Grimaldi <anthgrim@icloud.com>
2023-02-28 16:36:20 -0500

    fixing async error handling, adding depl congif for mongodb

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import 'express-async-errors'
 import { currentuserRouter } from './routes/current-user'
 import { siginRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
@@ -16,7 +17,7 @@ app.use(siginRouter)
 app.use(signoutRouter)
 app.use(signupRouter)
 
-app.all('*', () => {
+app.all('*', async (req, res) => {
   throw new NotFoundError()
 })
 
