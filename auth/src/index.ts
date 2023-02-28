0622eb371d56e9251f5090c121e66a9e68e3df4d
Kevin Grimaldi <anthgrim@icloud.com>
2023-02-28 16:46:05 -0500

    adding mongoose and connecting to mongodb pod

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import 'express-async-errors'
+import mongoose from 'mongoose'
 import { currentuserRouter } from './routes/current-user'
 import { siginRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
@@ -25,6 +26,17 @@ app.use(errorHandler)
 
 process.on('uncaughtException', (err) => console.log('Error:', err.message))
 
-app.listen(3000, () => {
-  console.log('Auth Service running on port:3000')
-})
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+    console.log('Connected to mongodb')
+  } catch (error) {
+    console.error(error)
+  }
+
+  app.listen(3000, () => {
+    console.log('Auth Service running on port:3000')
+  })
+}
+
+start()
