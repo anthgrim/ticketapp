73dce9e39da9ba80cc8854efa2ab08720ae3ec7b
Kevin Grimaldi <anthgrim@icloud.com>
2023-02-28 21:28:01 -0500

    adding sigin logic, and request validation middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,45 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import jwt from 'jsonwebtoken'
+import { validateRequest } from '../middlewares/validate-request'
+import { User } from '../models/user'
+import { Password } from '../services/password'
+import { body } from 'express-validator'
+import { BadRequestError } from '../errors/bad-request-error'
 
 const router = express.Router()
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('Signing in')
-})
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().notEmpty().withMessage('You must supply a password')
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body
+
+    const targetUser = await User.findOne({ email }).exec()
+
+    if (!targetUser) throw new BadRequestError('User does not exist')
+
+    const isValid = await Password.compare(targetUser.password, password)
+
+    if (!isValid) throw new BadRequestError('Password is not valid')
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      { id: targetUser.id, email: targetUser.email },
+      process.env.JWT_KEY!
+    )
+
+    req.session = {
+      jwt: userJwt
+    }
+
+    res.status(200).json({
+      user: targetUser
+    })
+  }
+)
 
 export { router as siginRouter }
