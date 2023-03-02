1a5c83f4b5be6d69fb84d5b00cb9644f09aafee4
Kevin Grimaldi <anthgrim@icloud.com>
2023-03-01 19:46:46 -0500

    moving current user to middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,19 +1,15 @@
 import express, { Request, Response } from 'express'
+import { currentUser } from '../middlewares/current-user'
 import jwt from 'jsonwebtoken'
 
 const router = express.Router()
 
-router.get('/api/users/currentuser', (req: Request, res: Response) => {
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null })
+router.get(
+  '/api/users/currentuser',
+  currentUser,
+  (req: Request, res: Response) => {
+    return res.send({ currentUser: req.currentUser || null })
   }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!)
-    res.send({ currentUser: payload })
-  } catch (error) {
-    return res.send({ currentUser: null })
-  }
-})
+)
 
 export { router as currentuserRouter }
