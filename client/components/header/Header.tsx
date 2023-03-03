dac803901e0b6a5402e30baf3f7544b5634a8127
Kevin Grimaldi <anthgrim@icloud.com>
2023-03-03 16:43:07 -0500

    adding header

--- client/components/header/Header.tsx
+++ client/components/header/Header.tsx
@@ -1,12 +1,45 @@
-import { CurrentUser } from '@/types'
 import React from 'react'
+import Link from 'next/link'
+import { CurrentUser } from '@/types'
 
 interface Props {
   currentUser: CurrentUser | null
 }
 
+interface ILink {
+  label: string
+  href: string
+  visible: boolean
+}
+
 const Header = ({ currentUser }: Props) => {
-  return <div>Hello! {currentUser?.email}</div>
+  const links: ILink[] = [
+    { label: 'Sign Up', href: '/auth/signup', visible: !currentUser },
+    { label: 'Sign In', href: '/auth/signin', visible: !currentUser },
+    { label: 'Sign Out', href: '/auth/signout', visible: currentUser !== null }
+  ]
+
+  const renderedLinks = links.map((link, index) => {
+    if (link.visible) {
+      return (
+        <Link key={index} className='nav-item' href={link.href}>
+          <div className='nav-link'>{link.label}</div>
+        </Link>
+      )
+    }
+  })
+
+  return (
+    <nav className='navbar navbar-light bg-light'>
+      <Link href='/' className='navbar-brand'>
+        GitTix
+      </Link>
+
+      <div className='d-flex justify-content-end'>
+        <ul className='nav d-flex align-items-center'>{renderedLinks}</ul>
+      </div>
+    </nav>
+  )
 }
 
 export default Header
